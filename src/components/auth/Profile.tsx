f27f9dc90bd856f81ced708f3077b206e30fea4f
Gopi Savaliya <gopis@Gopis-MacBook-Air.local>
2022-03-08 16:02:05 +0530

    Basic featured files added

---
+++ src/components/auth/Profile.tsx
@@ -0,0 +1,11 @@
+export type ProfileProps = {
+    name: string
+}
+
+function Profile({ name } : ProfileProps ) {
+  return (
+    <div>Private profile component. Name is { name }</div>
+  )
+}
+
+export default Profile
